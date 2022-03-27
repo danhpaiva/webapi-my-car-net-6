b6a9e48f0014aec59ce59ab5f6f927bf412cdd55
Paiva <danpaiva@live.com>
2022-03-27 17:45:01 -0300

    add código | alterar a string de conexao quando for testar

---
+++ MyCarApi/Context/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyCarApi.Models;
+
+namespace MyCarApi.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+                var connectionString = configuration.GetConnectionString("ServerConnection");
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+: base(options)
+        { }
+    }
+}
--- MyCarApi/Program.cs
+++ MyCarApi/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using MyCarApi.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+IConfiguration configuration;
+builder.Services.AddDbContext<AppDbContext>(option => option.UseSqlServer("Password=12345;Persist Security Info=True;User ID=sa;Initial Catalog=MyCar;Data Source=DESKTOP-IQUUNOA"));
 
 var app = builder.Build();
 
